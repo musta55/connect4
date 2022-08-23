c5a2fc39fede5c0319fc99391e91f7d64eaff390
musta55 <bsse1114@iit.du.ac.bd>
2022-08-24 01:31:08 +0600

    Done basic  utility functions

--- game/src/app/ui/ui.component.ts
+++ game/src/app/ui/ui.component.ts
@@ -12,11 +12,14 @@ export class UIComponent implements OnInit {
 
   constructor() { }
   visible = true;
-  HUMAN = 0;
-  AI = 1;
-  BLANK = 2;
-  i: number=0;
-  isFilled:boolean[] = new Array(42);
+  HUMAN = 1;
+  AI = 2;
+  EMPTY = 0;
+  windowLength = 4;
+  COL_COUNT = 7;
+  ROW_COUNT = 6;
+  i: number = 0;
+  isFilled: boolean[] = new Array(42);
   currentTurnNumber: number = 0;
 
   rowIndextoSit: number[] = new Array(7);
@@ -27,11 +30,40 @@ export class UIComponent implements OnInit {
 
   tiles = document.getElementsByClassName('inner-circle');
 
+   board = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0]
+  ]
+
+  testBoard = [
+    [0,0,0,0,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,2,2,0,1,1,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0],
+    [0,0,1,0,0,0,0]
+  ]
+
   ngOnInit(): void {
-    for(this.i=0;this.i<42;this.i++){
+    
+    var counter= this.countPiece(this.testBoard[1],1);
+    console.log("Counter holo " +counter)
+    for (this.i = 0; this.i < 42; this.i++) {
       this.isFilled[this.i] = false;
       this.whoseTurn[this.i] = 'player';
     }
+
+    console.log("Winning ",this.winningMove(this.testBoard,1))
+
+    console.log("Terminal check", this.isTerminal(this.testBoard))
+
+    console.log("Score Check ", this.scoreForCurrentPosition(this.testBoard,this.HUMAN))
+
+
 
     this.rowIndextoSit[0] = 35;
     this.rowIndextoSit[1] = 36;
@@ -41,30 +73,33 @@ export class UIComponent implements OnInit {
     this.rowIndextoSit[5] = 40;
     this.rowIndextoSit[6] = 41;
   }
-  
+
   tilesNumber: number = 42;
 
-  
+
 
   counter(i: number) {
     return new Array(i);
   }
 
-  changeColour(i: number){
-    console.log("The position is "+ i);
+  changeColour(i: number) {
+    console.log("The position is " + i);
 
-    var row = Math.floor(i/7);
-    var col = i%7;
-    console.log("Row and Column " + row + " " +  col + " " + this.rowIndextoSit[col]);
+    var row = Math.floor(i / 7);
+    var col = i % 7;
+    
+    console.log("Row and Column " + row + " " + col + " " + this.rowIndextoSit[col]);
 
-    if(this.currentTurnPlayer){
+    if (this.currentTurnPlayer) {
       this.currentTurnPlayer = false;
       this.whoseTurn[this.rowIndextoSit[col]] = 'player';
+      this.board[row] [col] =this.HUMAN;
       console.log("Next turn is bot");
     }
-    else{
+    else {
       this.currentTurnPlayer = true;
       this.whoseTurn[this.rowIndextoSit[col]] = 'bot';
+      this.board[row] [col] =this.AI;
       console.log("Next turn is human");
     }
 
@@ -72,17 +107,219 @@ export class UIComponent implements OnInit {
     this.rowIndextoSit[col] -= 7;
 
     this.currentTurnNumber++;
-    
+
   }
 
-  setStarter(input: number){
+  setStarter(input: number) {
     this.visible = false;
-    if(input==0){
+    if (input == 0) {
       this.currentTurnPlayer = true;
     }
-    else{
+    else {
       this.currentTurnPlayer = false;
     }
   }
+
+
+  winningMove(board: number[][], piece: number) {
+    for (var c = 0; c < this.COL_COUNT - 3; c++) {
+      for (var r = 0; r < this.ROW_COUNT; r++) {
+        //horizontal check
+        if (board[r][c] == piece && board[r][c + 1] == piece && board[r][c + 2] == piece && board[r][c + 3] == piece) {
+          return true;
+        }
+      }
+    }
+
+    //vertical move
+    for (var r = 0; r < this.ROW_COUNT - 3; r++) {
+      for (var c = 0; c < this.COL_COUNT; c++) {
+        if (board[r][c] == piece && board[r + 1][c] == piece && board[r + 2][c] == piece && board[r + 3][c] == piece) {
+          return true;
+        }
+      }
+    }
+
+
+    //pos diagonal move
+    for (var r = 0; r < this.ROW_COUNT - 3; r++) {
+      for (var c = 0; c < this.COL_COUNT - 3; c++) {
+        if (board[r][c] == piece && board[r + 1][c + 1] == piece && board[r + 2][c + 2] == piece && board[r + 3][c + 3] == piece) {
+          return true;
+        }
+      }
+    }
+
+    //neg diagonal move
+    for (var c = 0; c < this.COL_COUNT - 3; c++) {
+      for (var r = 3; r < this.ROW_COUNT; r++) {
+        if (board[r][c] == piece && board[r - 1][c + 1] == piece && board[r - 2][c + 2] == piece && board[r - 3][c + 3] == piece) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  isValidLocation(board: number[][], col: number) {
+    return board[this.ROW_COUNT - 1][col] == this.EMPTY;
+  }
+
+  getNextOpenRow(board: number[][], col: number) {
+    for (var r = 0; r < this.ROW_COUNT; r++) {
+      if (board[r][col] == this.EMPTY) {
+        return r;
+      }
+    }
+    return 19;
+  }
+
+  countPiece(pieces: number[], target: number) {
+    var count = 0;
+    pieces.forEach(value => {
+      if (target == value) {
+        count += 1;
+      }
+    });
+
+
+    return count;
+  }
+
+  evaluateWindow(window: number[], curPlayer: number) {
+    var score = 0;
+
+
+    var opponentPiece = this.HUMAN;
+
+    if (curPlayer == this.HUMAN) {
+      opponentPiece = this.AI;
+
+    }
+
+    var curPlayerCount = this.countPiece(window, curPlayer);
+
+    var emptyCount = this.countPiece(window, this.EMPTY);
+    var opponentCount = this.countPiece(window, opponentPiece);
+
+
+    if (curPlayerCount == 4) score += 100;
+    else if (curPlayerCount == 3 && emptyCount == 1) score += 5;
+    else if (curPlayerCount == 2 && emptyCount == 2) score += 2;
+    else if (opponentCount == 3 && emptyCount == 1) score -= 5;
+
+    return score;
+
+  }
+
+  scoreForCurrentPosition(board: number[][], curPlayer: number) {
+    var centerArray = [];
+    var score = 0;
+    for (var r = 0; r < this.ROW_COUNT; r++) {
+      centerArray.push(board[r][this.COL_COUNT / 2]);
+
+    }
+
+    var centerPieceCount = 0;
+    centerPieceCount = this.countPiece(centerArray, curPlayer);
+    score += centerPieceCount * 3;
+
+
+    //For horizontal array 
+
+
+    for (var r = 0; r < this.ROW_COUNT; r++) {
+      for (var c = 0; c < this.COL_COUNT - 3; c++) {
+        var horizontalArray = [];
+
+        horizontalArray.push(board[r][c]);
+        horizontalArray.push(board[r][c + 1]);
+        horizontalArray.push(board[r][c + 2]);
+        horizontalArray.push(board[r][c + 3]);
+
+
+        score += this.evaluateWindow(horizontalArray, curPlayer);
+
+      }
+    }
+
+
+    // For vertical array
+
+    for (var r = 0; r < this.ROW_COUNT - 3; r++) {
+      for (var c = 0; c < this.COL_COUNT; c++) {
+        var verticalArray = [];
+
+        verticalArray.push(board[r][c]);
+        verticalArray.push(board[r + 1][c]);
+        verticalArray.push(board[r + 2][c]);
+        verticalArray.push(board[r + 3][c]);
+
+
+        score += this.evaluateWindow(verticalArray, curPlayer);
+
+      }
+    }
+
+    // For  pos Diagonal array 
+
+    for (var r = 0; r < this.ROW_COUNT - 3; r++) {
+      for (var c = 0; c < this.COL_COUNT - 3; c++) {
+        var posDiagonalArray = [];
+
+        posDiagonalArray.push(board[r][c]);
+        posDiagonalArray.push(board[r + 1][c + 1]);
+        posDiagonalArray.push(board[r + 2][c + 2]);
+        posDiagonalArray.push(board[r + 3][c + 3]);
+
+
+        score += this.evaluateWindow(posDiagonalArray, curPlayer);
+
+      }
+    }
+
+
+    // For neg diagonal array
+    for (var r = 0; r < this.ROW_COUNT - 3; r++) {
+      for (var c = 0; c < this.COL_COUNT - 3; c++) {
+        var posDiagonalArray = [];
+
+        posDiagonalArray.push(board[r][c + 3]);
+        posDiagonalArray.push(board[r + 1][c + 2]);
+        posDiagonalArray.push(board[r + 2][c + 1]);
+        posDiagonalArray.push(board[r + 3][c]);
+
+
+        score += this.evaluateWindow(posDiagonalArray, curPlayer);
+
+      }
+    }
+
+    return score;
+
+
+  }
+
+
+  validMove(board:number[][]){
+    var validLocation=[];
+
+    for(var c=0;c< this.COL_COUNT; c++){
+      if(this.isValidLocation(board,c)){
+        validLocation.push(c);
+      }
+    }
+
+    return validLocation;
+
+  }
+
+  
+  isTerminal(board: number[][]) {
+    return this.winningMove(board,this.HUMAN) || this.winningMove(board,this.AI) || this.validMove(board).length==0;
+  }
+
 
 }
