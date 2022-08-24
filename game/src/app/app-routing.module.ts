b5484cb262daa2c83a5464fd7f00314df090d432
TasmiaZerin1128 <bsse1128@iit.du.ac.bd>
2022-08-24 13:05:28 +0600

    start page

--- game/src/app/app-routing.module.ts
+++ game/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
 import { UIComponent } from './ui/ui.component';
 
 const routes: Routes = [
-  { path: 'game', component: UIComponent }
+  { path: 'game', component: UIComponent },
+  { path: '', component: StartComponent }
 ];
 
 @NgModule({
